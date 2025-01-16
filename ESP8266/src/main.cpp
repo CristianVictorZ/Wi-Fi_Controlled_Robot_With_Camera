33315b91e76728a35dc7ae3373e86569f9ab703b
CristianVictorZ <148216241+CristianVictorZ@users.noreply.github.com>
2025-01-16 13:25:20 +0200

    Update main.cpp

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -2,13 +2,20 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <SoftwareSerial.h>
+#include <PID.h>
 
 #define SRX D5
 #define STX D6
 
 Adafruit_PWMServoDriver servoDriver;
 SoftwareSerial espSerial(SRX, STX);
+Adafruit_MPU6050 mpu;
+#define PID_KP 0.2
+#define PID_KD 0.02
+#define PID_KI 0.001
 
 char MOVEMENT_CONTROL = 0b00000001;
 #define MOVEMENT_ENABLED 7
@@ -16,6 +23,7 @@ char MOVEMENT_CONTROL = 0b00000001;
 #define MOVEMENT_SELECT 5
 #define MOVEMENT_FORWARD 4
 #define MOVEMENT_ROTATION 3
+#define MOVEMENT_CORRECTION 2
 
 #define NUM_SERVOS 8
 #define SERVO_MIN 200
@@ -23,6 +31,9 @@ char MOVEMENT_CONTROL = 0b00000001;
 const int servoPins[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 
 int forwardSwitch = 0, backwardSwitch = 0, leftSwitch = 0, rightSwitch = 0;
+
+const double timeStep = 1.0 / 5.0;
+PID *pitchPid = new PID(PID_KP, PID_KD, PID_KI, timeStep);
 
 //0: min - to its right (facing forward); max - to its left
 //1: min - upward; max - downward
@@ -34,7 +45,8 @@ int forwardSwitch = 0, backwardSwitch = 0, leftSwitch = 0, rightSwitch = 0;
 //7: same as 5
 
 const int idleValues[NUM_SERVOS] = {350, 350, 350, 350, 350, 350, 350, 350};
-const int errorValues[NUM_SERVOS] = {20, -70, -25, -75, -10, -10, -70, -10};
+const int errorValues[NUM_SERVOS] = {50, -70, -10, -70, 10, 10, -50, 10};
+int correctionValues[NUM_SERVOS/2] = {0, 0, 0, 0};
 unsigned long lastValueLoad = 0;
 #define TEST_VALUES_WAIT 4000
 const int testValues[8][NUM_SERVOS] = {{250, 250, 250, 250, 250, 250, 250, 250},
@@ -86,10 +98,21 @@ unsigned long lastDriverUpdate = 0;
 
 void updateServo(const int newValues[NUM_SERVOS])
 {
-  for(int i=0; i<NUM_SERVOS; i++)
+  if(MOVEMENT_CONTROL & (1<<MOVEMENT_CORRECTION))
   {
-    servoDriver.setPWM(servoPins[i], 0, newValues[i] + errorValues[i]);
-  }
+    Serial.print("DA");
+    for(int i=0; i<NUM_SERVOS; i++)
+    {
+      if(i%2==1)
+        servoDriver.setPWM(servoPins[i], 0, newValues[i] + errorValues[i] + correctionValues[i/2]);
+      else
+        servoDriver.setPWM(servoPins[i], 0, newValues[i] + errorValues[i]);
+    }
+  }else
+    for(int i=0; i<NUM_SERVOS; i++)
+    {
+      servoDriver.setPWM(servoPins[i], 0, newValues[i] + errorValues[i]);
+    }
 }
 
 void movementTest()
@@ -185,6 +208,7 @@ void setup() {
   espSerial.begin(115200);
 
   Wire.begin();
+  mpu.begin();
   
   servoDriver.begin();
   servoDriver.setPWMFreq(50);
@@ -193,9 +217,17 @@ void setup() {
   Serial.println("Setup complete!");
 
   movementIdle();
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 }
 
+double roll = 0.00, pitch = 0.00;
+
 void loop() {
+
+  sensors_event_t a, g, temp;
   
   if(millis() - lastDriverUpdate > DRIVER_UPDATE_DELAY)
   {
@@ -233,6 +265,54 @@ void loop() {
         }
       }
     }
+    mpu.getEvent(&a, &g, &temp);
+
+    roll = (atan2(a.acceleration.y , a.acceleration.z) * 57.3 + 90) * 2.77 + 100;
+    pitch = (atan2((- a.acceleration.x) , sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 57.3 + 90) * 2.77 + 100;
+
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.print(" m/s^2");
+    Serial.print("; Rotation roll: ");
+    Serial.print(roll);
+    Serial.print(", pitch: ");
+    Serial.println(pitch);
+
+    if(MOVEMENT_CONTROL & (1<<MOVEMENT_CORRECTION))
+    {
+      Serial.print("DA");
+      double error = pitch - 350.0;
+      float adjustment = (float)pitchPid->Control(error);
+      if(correctionValues[0] > 50)
+      {
+        MOVEMENT_CONTROL &= !(1<<MOVEMENT_CORRECTION);
+        for(int i=0; i<4; i++)
+        {
+          correctionValues[i] = 0;
+        }
+      }else if(abs(error) > 5)
+      {
+        for(int i=0; i<4; i++)
+        {
+          correctionValues[i] = correctionValues[i] + adjustment;//TO CHECK
+          //correctionValues[i] = adjustment;
+          Serial.print(correctionValues[i]);
+          Serial.print(" ");
+        }
+      }
+    }else
+    {
+      pitchPid->Reset();
+      for(int i=0; i<4; i++)
+      {
+        correctionValues[i] = 0;
+      }
+    }
+    Serial.println(" ");
   }
 
   if(espSerial.available())
